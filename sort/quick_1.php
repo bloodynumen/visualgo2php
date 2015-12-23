da23f72e6570e826a0894828eb12bc73c773f4a1
孙槐 <sunhuai@zplay.cn>
2015-12-23 12:44:02 +0900

    update sort

--- sort/quick_1.php
+++ sort/quick_1.php
@@ -1,32 +1,55 @@
 <?php
 
-$list = [];
-
-function quick($list, $left = 0, $right = 0)
+/*
+ * @brief 从左侧段取基准值,通过交换位置,来达到排序的目的
+ * 
+ * @param array $list 需要排序的列表
+ * @param int $left 左侧起始位置
+ * @param int $right 右侧起始位置
+ *
+ * @return null
+ */
+function quick(&$list, $left = 0, $right = 0)
 {
     if ($left >= $right)
     {
-        break;
+        return;
     }
     $base = $list[$left];
-    $begin = $left;
-    $end = $right;
+    $i = $left;
+    $j = $right;
 
     while($j != $i)
     {
-        while ($i < $i && $list[$j] > $base)
+        # 先找小于base的值
+        while ($i < $j && $list[$j] >= $base)
         {
             $j --;
         }
-        while ($i < $j && $list[$i] < $base)
+        # 再找大于base的值
+        while ($i < $j && $list[$i] <= $base)
         {
             $i ++;
         }
-        list($list[$i], $list[$j]) = [$list[$j], $list[$i]];
+        if ($j != $i)
+        {
+            $tmp = $list[$i];
+            $list[$i] = $list[$j];
+            $list[$j] = $tmp;
+        }
     }
 
-    list($list[$left], $list[$j]) = [$list[$j], $base];
-    quick($left, $j - 1);
-    quick($j + 1, $right);
+    # 相遇时的值 一定小于或等于base
+    $tmp = $list[$j];
+    $list[$j] = $base;
+    $list[$left] = $tmp;
+
+    quick($list, $left, $j - 1);
+    quick($list, $j + 1, $right);
 }
 
+$list = [3, 5, 1, 2, 8, 9, 13, 123, 67, 123, 5345, 2134123, 1231, 23, 4, 5, 67, 32, 1, 0];
+print_r($list);
+
+quick($list, 0, count($list) - 1);
+print_r($list);
--- sort/quick_2.php
+++ sort/quick_2.php
@@ -1,20 +1,29 @@
 <?php
 
-function quick($arr = [])
+/*
+ * @brief 从左侧段取基准值,通过递归合并数组,来排序
+ * 
+ * @param array $list 需要排序的列表
+ *
+ * @return array 排序后的列表
+ */
+function quick($list = [])
 {
-    if (count($arr) <= 1)
+    if (count($list) <= 1)
     {
-        return $arr;
+        return $list;
     }
 
-    $base = $arr[0];
+    $base = array_shift($list);
     $left = [];
     $right = [];
 
-    foreach ($arr as $val)
+    foreach ($list as $val)
     {
         $val > $base ? $right[] = $val : $left[] = $val;
     }
-
-    return array_merge(quick($left), [$base], $quick($right));
+    return array_merge(quick($left), (array)$base, quick($right));
 }
+
+$list = [3, 5, 1, 2, 8, 9, 13, 123, 67, 123, 5345, 2134123, 1231, 23, 4, 5, 67, 32, 1, 0];
+print_r(quick($list));
